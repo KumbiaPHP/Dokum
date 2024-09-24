c333521d4d99e0b0172a520d02949da367586fe6
Henry Stivens Adarme Muñoz <henry.stivens@gmail.com>
2024-09-24 18:19:55 -0500

    Refactor `VersionControlFactory` to use `str_contains`.
    Replace `strpos` with `str_contains` for better readability and modern PHP. Updated exceptions documentation to improve clarity on error handling in `createFromUrl`.

--- dokum/app/libs/VersionControl/VersionControlFactory.php
+++ dokum/app/libs/VersionControl/VersionControlFactory.php
@@ -5,21 +5,28 @@ namespace Dokum\libs\VersionControl;
 use Dokum\libs\VersionControl\Adapters\Github as AdaptersGithub;
 use Dokum\libs\VersionControl\Adapters\Gitlab as AdaptersGitlab;
 use Dokum\libs\VersionControl\Exceptions\UnrecognizedSourceException;
+use Exception;
 
+/**
+ * The VersionControlFactory class is responsible for creating instances
+ * of version control adapter classes based on the given repository URL.
+ */
 class VersionControlFactory
 {
     /**
      * Detects the source (GitHub or GitLab) and returns the appropriate class instance.
      *
      * @param string $url The repository URL
+     *
      * @return VersionControlInterface The appropriate class instance
      * @throws Exception If the source is not recognized
+     * @throws UnrecognizedSourceException If the adapter for service does not found
      */
     public static function createFromUrl(string $url): VersionControlInterface
     {
-        if (strpos($url, 'github.com') !== false) {
+        if (str_contains($url, 'github.com')) {
             return new AdaptersGithub();
-        } elseif (strpos($url, 'gitlab.com') !== false) {
+        } elseif (str_contains($url, 'gitlab.com')) {
             return new AdaptersGitlab();
         } else {
             throw new UnrecognizedSourceException($url);
