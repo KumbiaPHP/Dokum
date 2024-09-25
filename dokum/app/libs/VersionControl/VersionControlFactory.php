31dc3f09cbd7a04cee9a712c570f91fb70d50416
Joan Miquel <joanhey@kumbiaphp.com>
2024-09-25 03:35:31 +0200

    Remove extra use in VersionControlFactory.php

--- dokum/app/libs/VersionControl/VersionControlFactory.php
+++ dokum/app/libs/VersionControl/VersionControlFactory.php
@@ -2,11 +2,6 @@
 
 namespace Dokum\libs\VersionControl;
 
-use Dokum\libs\VersionControl\Adapters\Github as AdaptersGithub;
-use Dokum\libs\VersionControl\Adapters\Gitlab as AdaptersGitlab;
-use Dokum\libs\VersionControl\Exceptions\UnrecognizedSourceException;
-use Exception;
-
 /**
  * The VersionControlFactory class is responsible for creating instances
  * of version control adapter classes based on the given repository URL.
@@ -25,11 +20,11 @@ class VersionControlFactory
     public static function createFromUrl(string $url): VersionControlInterface
     {
         if (str_contains($url, 'github.com')) {
-            return new AdaptersGithub();
+            return new Adapters\Github();
         } elseif (str_contains($url, 'gitlab.com')) {
-            return new AdaptersGitlab();
-        } else {
-            throw new UnrecognizedSourceException($url);
+            return new Adapters\Gitlab();
         }
+        
+        throw new Exceptions\UnrecognizedSourceException($url);
     }
 }
